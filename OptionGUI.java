55b19af1ae2616525463603e2205636d7a44bc9b
Fyping <18779875304@163.com>
2016-10-24 20:58:11 +0800

    Update OptionGUI.java

--- OptionGUI.java
+++ OptionGUI.java
@@ -1,4 +1,4 @@
-package GitBub;
+}package GitBub;
 import javax.swing.JOptionPane;
 /***
  * @author:FangYanPing   from JXAU ,in China;
@@ -9,6 +9,13 @@ import javax.swing.JOptionPane;
  * @param:tx,提供在框架中显示的提示信息,
  * @param:str,返回的字符串信息
  * **/
+/*
+Function: to create a visual framework to display a view, 
+* enter a string from the keyboard, 
+* the class returns the character from the keyboard by using the GetString function. 
+* @param:tx, provide prompt information displayed in the frame, 
+* @param:str, string information returned 
+*/
 public class OptionGUI {
 	String text ;
 	public OptionGUI(String tx){
@@ -18,11 +25,21 @@ public class OptionGUI {
 		return text;
 	}
 	
-	public static void main(String[] args) {
-		//This is test infomation:
-		/*OptionGUI gui = */;
-		String str= new OptionGUI("Please input issue of what you want: ").getInputInfo();
-		System.out.println(str);
-	}
-
 }
+
+//This is another class which to test it!
+//
+public class MainTest{	
+	public static void main(String[] args) {
+			//This is test infomation:
+			/*OptionGUI gui = */;
+			String str= new OptionGUI("Please input issue of what you want: ").getInputInfo();
+			System.out.println(str);
+		}
+}
+/*
+Originally, it should be stored in another file, 
+	but I think all the code written to the same file, 
+	more conducive to future maintenance and code optimization, 
+	after all, this is just an ordinary class not to write English project,
+	in order to prevent garbled */
